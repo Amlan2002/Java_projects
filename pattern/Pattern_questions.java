429699c1af493e7c9267f11d7b3650464c9a28ad
Amlan2002 <iamamlan2002@gmail.com>
2022-06-16 14:28:37 +0530

    pattern question

--- pattern/Pattern_questions.java
+++ pattern/Pattern_questions.java
@@ -1,3 +1,4 @@
+package pattern;
 public class Pattern_questions {
     public static void main(String[] args) {
 
---
+++ pattern/pattern_question2.java
@@ -0,0 +1,36 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class pattern_question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row, left, col,right;
+        for (row = n; row >= 1; row--) {
+            for (left = n; left>row; left--) {
+                System.out.print(left);
+            }
+            for (col = 1; col <= 2 * row - 1; col++) {
+                System.out.print(row);
+            }
+            for (right = row+1; right<=n; right++) {
+                System.out.print(right);
+            }
+            System.out.println();
+        }
+        for (row = 2; row <= n; row++) {
+            for (left = n; left>row; left--) {
+                System.out.print(left);
+            }
+            for (col = 1; col <= 2 * row - 1; col++) {
+                System.out.print(row);
+            }
+            for (right = row+1; right<=n; right++) {
+                System.out.print(right);
+            }
+            System.out.println();
+        }
+
+    }
+}
